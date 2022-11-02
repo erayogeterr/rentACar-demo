2e23d1760c7af6288d47b3cad1a102e96b36b828
Eray Ögeter <erayogeterr@gmail.com>
2022-11-02 23:02:55 +0300

    added request response dto's

--- src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -2,10 +2,11 @@ package kodlama.io.rentACar.business.concretes;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
@@ -14,14 +15,21 @@ public class BrandManager implements BrandService {
 
 	private final BrandRepository brandRepository;
 
-	@Autowired
 	public BrandManager(BrandRepository brandRepository) {
 		this.brandRepository = brandRepository;
 	}
 
 	@Override
-	public List<Brand> getAll() {
-		return brandRepository.findAll();
+	public List<GetAllBrandsResponse> getAll() {
+		//return brandRepository.findAll();
+		return null;
+	}
+
+	@Override
+	public void add(CreateBrandRequest createBrandRequest) {
+		Brand brand = new Brand();
+		brand.setName(createBrandRequest.getName());
+		this.brandRepository.save(brand);
 	}
 
 }
