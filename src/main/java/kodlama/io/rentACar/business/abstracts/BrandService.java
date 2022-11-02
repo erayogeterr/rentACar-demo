d3681d07fe49b2b6082bc2699bbd1bcb7bc4cef2
Eray Ögeter <erayogeterr@gmail.com>
2022-11-02 21:59:52 +0300

    added postgreSQL

--- src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
+++ src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -5,9 +5,7 @@ import java.util.List;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
 public interface BrandService {
-	
+
 	List<Brand> getAll();
-	
-	
 
 }
--- src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -21,7 +21,7 @@ public class BrandManager implements BrandService {
 
 	@Override
 	public List<Brand> getAll() {
-		return brandRepository.getAll();
+		return brandRepository.findAll();
 	}
 
 }
